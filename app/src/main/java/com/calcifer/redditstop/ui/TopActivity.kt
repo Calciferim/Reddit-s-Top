8a006ef9bb77f8fee0058746d965bb314ec515b6
Calcifer <03081992@mail.ru>
2018-09-30 21:03:01 +0300

    initialization

---
+++ app/src/main/java/com/calcifer/redditstop/ui/TopActivity.kt
@@ -0,0 +1,13 @@
+package com.calcifer.redditstop.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.calcifer.redditstop.R
+
+class TopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+    }
+}
